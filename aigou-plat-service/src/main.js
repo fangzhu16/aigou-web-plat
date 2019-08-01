ce3a2bea8f75517d85b723ab53c86bddb2a1eb44
fangzhu16 <1982977205@qq.com>
2019-08-01 09:03:32 +0800

    网页1

---
+++ aigou-plat-service/src/main.js
@@ -0,0 +1,61 @@
+import babelpolyfill from 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+//import './assets/theme/theme-green/index.css'
+import VueRouter from 'vue-router'
+import store from './vuex/store'
+import Vuex from 'vuex'
+//import NProgress from 'nprogress'
+//import 'nprogress/nprogress.css'
+import routes from './routes'
+//禁用mock
+// import Mock from './mock'
+// Mock.bootstrap();
+import 'font-awesome/css/font-awesome.min.css'
+
+// 引用axios，并设置基础URL为后端服务api地址
+var axios = require('axios')
+axios.defaults.baseURL = 'http://127.0.0.1:4000/services'  //对应后端网关统一地址
+// 将API方法绑定到全局  /plat/login
+Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+//NProgress.configure({ showSpinner: false });
+
+const router = new VueRouter({
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    //NProgress.start();
+    if (to.path == '/login') {
+        sessionStorage.removeItem('user');
+    }
+    let user = JSON.parse(sessionStorage.getItem('user'));
+    if (!user && to.path != '/login') {
+        next({path: '/login'})
+    } else {
+        next()
+    }
+})
+
+//router.afterEach(transition => {
+//NProgress.done();
+//});
+
+new Vue({
+    //el: '#app',
+    //template: '<App/>',
+    router,
+    store,
+    //components: { App }
+    render: h => h(App)
+}).$mount('#app')
+
